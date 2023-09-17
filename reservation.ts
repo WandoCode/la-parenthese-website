382f4a6dcd5710a99ccdfe8a3a0b56ebfba643cd
Maxime Chirez <mchirez@gmail.com>
2023-09-17 12:08:13 +0200

    Add a date picker for Date and Time input fields

--- reservation.ts
+++ reservation.ts
@@ -1,11 +1,26 @@
+import AirDatepicker from 'air-datepicker'
+import localeFr from 'air-datepicker/locale/fr'
+import 'air-datepicker/air-datepicker.css'
+
 function main() {
+  new AirDatepicker('#date', {
+    selectedDates: [new Date()],
+    locale: localeFr,
+  })
+
+  new AirDatepicker('#heure', {
+    locale: localeFr,
+    timepicker: true,
+    minHours: 9,
+    maxHours: 18,
+    minutesStep: 15,
+    onlyTimepicker: true,
+  })
   getURLParams()
 }
 
 function getURLParams() {
   const url = new URL(window.location.href)
-
-  console.log(url)
 }
 
 main()
