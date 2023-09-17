7109618d06b251f6c7200ab6aef28cfa8896b108
Maxime Chirez <mchirez@gmail.com>
2023-09-17 12:19:33 +0200

    When user comes to the reservation page by clicking on a 'reserver' button in the massages page, the select massage input is prefilled accordingly

--- reservation.ts
+++ reservation.ts
@@ -1,8 +1,12 @@
 import AirDatepicker from 'air-datepicker'
 import localeFr from 'air-datepicker/locale/fr'
 import 'air-datepicker/air-datepicker.css'
+const massageChoiceForm = document.getElementById(
+  'massage-choice-form'
+) as HTMLFormElement
 
 function main() {
+  fillMassageInputFromURLParams()
   new AirDatepicker('#date', {
     selectedDates: [new Date()],
     locale: localeFr,
@@ -16,11 +20,22 @@ function main() {
     minutesStep: 15,
     onlyTimepicker: true,
   })
-  getURLParams()
 }
 
-function getURLParams() {
+function fillMassageInputFromURLParams() {
+  const massageChoice = getMassageFromURLParams()
+
+  if (massageChoice) {
+    massageChoiceForm.elements['massage'].value = massageChoice
+  }
+}
+
+function getMassageFromURLParams() {
   const url = new URL(window.location.href)
+  const params = url.searchParams
+  const massage = params.get('massage')
+
+  return massage
 }
 
 main()
